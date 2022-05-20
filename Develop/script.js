e0099ac28200451b53bb3851dd0d2d4e853e3a82
okdavekk <okdavekk@gmail.com>
2022-05-20 18:07:03 -0400

    Commit after experimenting

--- Develop/script.js
+++ Develop/script.js
@@ -11,4 +11,43 @@ function writePassword() {
 }
 
 // Add event listener to generate button
+
+const element = document.getElementById("generate");
+
 generateBtn.addEventListener("click", writePassword);
+
+
+
+  
+//textarea id = "password"
+
+
+// button id = generate 
+
+alert()
+// alert(message)
+
+// confirm(message)
+
+// prompt()
+// prompt(message)
+// prompt(message, default)
+
+
+
+
+
+
+
+// 
+// 
+// 
+// 
+// 
+//Generator functions https://www.youtube.com/watch?v=duNmhKgtcsI
+
+// function getRandomLower() {
+//  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+// }
+
+// console.log(Math.floor(Math.random() * 26) + 97);
