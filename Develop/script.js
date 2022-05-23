003027453cfc49a1b806050e8a0614aa88044224
okdavekk <okdavekk@gmail.com>
2022-05-23 16:34:31 -0400

    Reverting to b23c732

--- Develop/script.js
+++ Develop/script.js
@@ -16,17 +16,8 @@ function generatePassword(){
   var SYMBOL_CHARACTERS = "!@#$%^&*(){}[]=<>/,.";
 
 
-// Can wrap it in "number" to get the consol log of length to turn from white to purple.  String to integer.  
-
-  var passwordLength = parseInt(prompt ("Password Length.  Select 8 through 128 Characters")); 
+  var passwordLength = window.prompt ("Password Length.  Select 8 through 128 Characters"); 
   console.log(passwordLength)
-  
-  if (passwordLength >= 8 && input <= 128){
-
-  } else {
-    alert("Please select a number between 8 and 128.");
-  }
-  
 
   var lowercaseCheck = window.confirm ("Would you like lowercase characters?");
   console.log(lowercaseCheck)
@@ -55,13 +46,14 @@ function generatePassword(){
     usableCharacters += SYMBOL_CHARACTERS;
   }
     console.log(usableCharacters)
-
-return "";
-
 }
 
 
+
+
 // Add event listener to generate button
+
+// const element = document.getElementById("generate");
 
 generateBtn.addEventListener("click", writePassword);
 
@@ -74,3 +66,69 @@ generateBtn.addEventListener("click", writePassword);
 
 
   
+//textarea id = "password"
+
+
+// button id = generate 
+
+// alert()
+// alert(message)
+
+// confirm(message)
+
+// prompt()
+// prompt(message)
+// prompt(message, default)
+
+
+
+
+
+
+
+// 
+// 
+// 
+// 
+// 
+//Code from this video https://www.youtube.com/watch?v=duNmhKgtcsI
+
+
+// Dom Elements
+// const resultPrompt = document.getElementById('result');
+// const lengthPrompt = document.getElementById('length');
+// const lowercasePrompt = document.getElementById('lowercase');
+// const uppercasePrompt = document.getElementById('uppercase');
+// const numericPromp = document.getElementById('numeric');const resultGenerate = document.getElementById('generate');
+
+// const randomFunc = {
+//   lower: getRandomLower,
+//   upper: getRandomUpper,
+//   number: getRandomNumber,
+//   symbol: getRandomSymbol
+// };
+
+// resultGenerate.addEventListener('click', () => {
+//   const length = lengthPrompt.value;
+//   const hasLower = lowercasePrompt.checked
+// }
+
+//Generator Functions
+// function getRandomLower() {
+//  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+// }
+
+// function getRandomUpper() {
+//   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+// }
+
+//  function getRandomNumber() {
+//   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+//  }
+
+//  function getRandomSymbol() {
+//   const symbols = '!@#$%^&*(){}[]=<>/,.';
+//   return symbols[Math.floor(Math.random() * symbols.length)];
+//  }
+
+
