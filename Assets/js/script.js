eddead9e4e2e6e0bf84b682a403d3784c4ed048f
okdavekk <okdavekk@gmail.com>
2022-05-23 22:55:34 -0400

    Commit where there is  some functionality point but still need to generate password

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -27,57 +27,54 @@ function generatePassword() {
   //VARIABLES AND WINDOWS
 
   //passwordLength VARIABLE AND WINDOWS
-  function lengthReturn() {
-    var passwordLength = parseInt(prompt("Password Length.  Select 8 through 128 Characters"));
-    console.log(passwordLength)
-    if (!passwordLength) {
-      alert("You must input a value from 8 through 128");
-      return lengthReturn();
-    }
-    if (passwordLength < 8) {
-      alert("You must select a number greater than or equal to 8");
-      return lengthReturn();
-    }
-    if (passwordLength > 128) {
-      alert("You must pick a number less than or equal to 128")
-      return lengthReturn();
-    }
+  var passwordLength = parseInt(prompt("Password Length.  Select 8 through 128 Characters"));
+  console.log(passwordLength)
+  if (!passwordLength) {
+    alert("You must input a value from 8 through 128.  Start over");
+    return generatePassword();
   }
-  lengthReturn()
+  if (passwordLength < 8) {
+    alert("You must select a number greater than or equal to 8.  Start over");
+    return generatePassword();
+  }
+  if (passwordLength > 128) {
+    alert("You must pick a number less than or equal to 128. Start over")
+    return generatePassword();
+  }
 
   //lowercaseCheck VARIABLE AND WINDOWS
-  function variable() {
-    var lowercaseCheck = window.confirm("You must select that you want to include lowercase characters");
-    console.log(lowercaseCheck)
-    if (!lowercaseCheck) {
-      alert("You must select that you want to include lowercase characters")
-      return variable();
-    }
+  var lowercaseCheck = window.confirm("You must select that you want to include lowercase characters");
+  console.log(lowercaseCheck)
+  if (!lowercaseCheck) {
+    alert("You must select that you want to include lowercase characters.  Start over")
+    return generatePassword();
   }
-  variable()
-
-
 
   //uppercaseCheck VARIABLE AND WINDOWS
   var uppercaseCheck = window.confirm("You must select that you would like uppercase characters");
   console.log(uppercaseCheck)
   if (!uppercaseCheck) {
-    alert("You must select that you would like uppercase characters")
+    alert("You must select that you would like uppercase characters.  Start over")
+    return generatePassword();
   }
 
   //numberCheck VARIABLE AND WINDOWS
   var numberCheck = window.confirm("You must select that you would like number characters");
   console.log(numberCheck)
   if (!numberCheck) {
-    alert("You must select that you would like number characters")
+    alert("You must select that you would like number characters. Start over")
+    return generatePassword();
   }
+
 
   //symbolCheck VARIABLE AND WINDOWS
   var symbolCheck = window.confirm("You must select that you would like symbol characters");
   console.log(symbolCheck)
-  if (!numberCheck) {
-    alert("You must select that you would like symbol characters")
+  if (!symbolCheck) {
+    alert("You must select that you would like symbol characters.  Start over")
+    return generatePassword();
   }
+
 
 
 
@@ -99,7 +96,19 @@ function generatePassword() {
   }
   console.log(usableCharacters)
 
-  return "";
+
+
+  //NEW FORMULA AFTER AGGREGATION
+  //NEW FORMULA AFTER AGGREGATION
+  //NEW FORMULA AFTER AGGREGATION
+  // var generatePassword = "";
+  // var characters = usableCharacters;
+  // var charactersLength = passwordLength;
+
+  // for (var i = 0; i < length; i++) {
+  //   result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  // }
+  // console.log(result);
 
 }
 
@@ -108,6 +117,9 @@ function generatePassword() {
 // Add event listener to generate button
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
 
 
 
